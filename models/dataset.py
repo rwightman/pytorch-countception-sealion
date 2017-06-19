Numerous improvements/fixes for training, preprocessing, added start of inference. * fix scaling of target impacting counts * load processed train data metadata and use for offsets of image/coords * add start of inference code * preprocessing now uses one npz for targets instead of int16 png

models/dataset.py
@@ -92,7 +92,10 @@ def find_inputs(folder, types=IMG_EXTENSIONS):
             if ext.lower() in types:
                 abs_filename = os.path.join(root, rel_filename)
                 inputs.append((int(base), abs_filename))
-    return zip(*sorted(inputs, key=lambda k: k[0]))
+    if inputs:
+        return zip(*sorted(inputs, key=lambda k: k[0]))
+    else:
+        return [], []
 
 
 def find_targets(folder, input_ids, types=IMG_EXTENSIONS):
@@ -103,12 +106,14 @@ def find_targets(folder, input_ids, types=IMG_EXTENSIONS):
             base, ext = os.path.splitext(rel_filename)
             if ext.lower() in types:
                 split = base.split('-')
-                if len(split) < 3:
-                    continue
                 fid = int(split[0])
                 if fid in inputs_set:
                     abs_filename = os.path.join(root, rel_filename)
-                    targets[fid][int(split[2])] = abs_filename
+                    print(split)
+                    if len(split) > 2:
+                        targets[fid][int(split[2])] = abs_filename
+                    else:
+                        targets[fid] = abs_filename
     return targets
 
 
@@ -161,14 +166,15 @@ class SequentialTileSampler(Sampler):
 
     def __init__(self, data_source):
         self.num_samples = len(data_source)
-        self.sample_tile_map = data_source.sample_tile_map
+        #self.sample_tile_map = data_source.sample_tile_map
         #FIXME not complete
 
     def __iter__(self):
         samples = range(self.num_samples)
-        for i in samples:
-            for t in self.sample_tile_map[i]:
-                yield SampleTileIndex(i, t)
+        return iter(samples)
+        #for i in samples:
+        #    for t in self.sample_tile_map[i]:
+        #        yield SampleTileIndex(i, t)
 
     def __len__(self):
         return self.num_samples # FIXME add up tiles
@@ -202,9 +208,11 @@ class SealionDataset(data.Dataset):
     def __init__(
             self,
             input_root,
-            target_root,
-            counts_file,
+            target_root='',
+            counts_file='',
             coords_file='',
+            processing_file='',
+            train=True,
             tile_size=[256, 256],
             transform=None,
             target_transform=None):
@@ -215,19 +223,40 @@ class SealionDataset(data.Dataset):
         self.input_index = input_ids
         self.data_by_id = {k: {'input': v} for k, v in zip(input_ids, input_paths)}
 
-        targets = find_targets(target_root, input_ids)
-        for k, v in targets.items():
-            d = self.data_by_id[k]
-            d['target'] = v
+        self.has_targets = False
+        if os.path.exists(target_root):
+            targets = find_targets(target_root, input_ids, types=['.npz'])
+            if len(targets):
+                for k, v in targets.items():
+                    d = self.data_by_id[k]
+                    d['target'] = v
+                self.has_targets = True
+            else:
+                raise (RuntimeError("Found 0 targets in : " + target_root))
+
+        if train:
+            assert self.has_targets
+        self.train = train
 
         if counts_file:
             counts_df = pd.read_csv(counts_file).rename(columns=CATEGORY_MAP)
             counts_df.drop(['train_id'], 1, inplace=True)
-            for k, v in counts_df.to_dict('index').items():
+            for k, v in counts_df.to_dict(orient='index').items():
                 if k in self.data_by_id:
                     d = self.data_by_id[k]
                     d['counts_by_cat'] = v
                     d['count'] = sum(v.values())
+
+        if processing_file:
+            process_df = pd.read_csv(processing_file, index_col=False)
+            cols = ['x_offset', 'y_offset', 'width', 'height']
+            process_df[cols] = process_df[cols].astype(int)
+            process_df['train_id'] = process_df.filename.map(lambda x: int(os.path.splitext(x)[0]))
+            process_df.set_index(['train_id'], inplace=True)
+            for k, v in process_df[cols].to_dict(orient='index').items():
+                if k in self.data_by_id:
+                    d = self.data_by_id[k]
+                    d['process'] = v
 
         if coords_file:
             coords_df = pd.read_csv(coords_file, index_col=False)
@@ -242,17 +271,26 @@ class SealionDataset(data.Dataset):
                 fid = int(os.path.splitext(file)[0])
                 if fid in self.data_by_id:
                     d = self.data_by_id[fid]
+                    if 'process' in d:
+                        xy_offset = np.array([d['process']['x_offset'], d['process']['y_offset']])
+                        coords[:, :2] = coords[:, :2] + xy_offset
                     d['coords'] = coords
 
         self.tile_size = tile_size
         self.dataset_mean = [0.43632373, 0.46022959, 0.4618598]
         self.dataset_std = [0.17749958, 0.16631233, 0.16272708]
         if transform is None:
-            self.transform = transforms.Compose([
-                transforms.ToTensor(),
-                mytransforms.ColorJitter(),
-                transforms.Normalize(self.dataset_mean, self.dataset_std)
-            ])
+            if self.train:
+                self.transform = transforms.Compose([
+                    transforms.ToTensor(),
+                    mytransforms.ColorJitter(),
+                    transforms.Normalize(self.dataset_mean, self.dataset_std)
+                ])
+            else:
+                self.transform = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize(self.dataset_mean, self.dataset_std)
+                ])
         self.target_transform = target_transform
 
     @functools.lru_cache(4)
@@ -269,13 +307,22 @@ class SealionDataset(data.Dataset):
     @functools.lru_cache(4)
     def _load_target(self, input_id):
         d = self.data_by_id[input_id]
-        tp = [cv2.imread(d['target'][x], -1) for x in range(5)]
-        target = np.dstack(tp)
-        target = target / np.iinfo(np.uint16).max
-        target = target.astype(np.float32, copy=False)
+        if isinstance(d['target'], dict):
+            tp = [cv2.imread(d['target'][x], -1) for x in range(5)]
+            target = np.dstack(tp)
+            target = target / np.iinfo(np.uint16).max
+            target = target.astype(np.float32, copy=False)
+        else:
+            target = np.load(d['target'])['arr_0']
+            target = target.astype(np.float32, copy=False)
         return target
 
-    def _random_tile_center(self, input_id, bbox):
+    def _indexed_tile_center(self, input_id, index):
+        d = self.data_by_id[input_id]
+        cx, cy = 0, 0
+        return cx, cy
+
+    def _random_tile_center(self, input_id, w, h):
         d = self.data_by_id[input_id]
         if len(d['coords']) and random.random() < 0.4:
             # 40% of the time, randomly pick a point around an actual sealion
@@ -284,20 +331,28 @@ class SealionDataset(data.Dataset):
             cy = cy + random.randint(-self.tile_size[1]//4, self.tile_size[1]//4)
         else:
             # return random center coords for specified tile size within a specified (x, y, w, h) bounding box
-            x_min = bbox[0]
-            x_max = bbox[0] + bbox[2]
-            y_min = bbox[1]
-            y_max = bbox[1] + bbox[3]
-            x_min += self.tile_size[0] // 2  # FIXME change to metadata border offsets
-            x_max -= self.tile_size[0] // 2
-            y_min += self.tile_size[1] // 2
-            y_max -= self.tile_size[1] // 2
+            tw, th = self.tile_size[0] // 2, self.tile_size[0]//2
+            x_min = tw
+            x_max = w - tw
+            y_min = th
+            y_max = h - th
+            if 'process' in d:
+                p = d['process']
+                xo, yo = p['x_offset'], p['y_offset']
+                ow, oh = p['width'], p['height']
+                x_min += xo
+                x_max -= w - ow - xo
+                y_min += yo
+                y_max -= h - oh - yo
+                #print(x_min, x_max, y_min, y_max, w, h, ow, oh)
             assert x_max - x_min > 0 and y_max - y_min > 0
             cx = random.randint(x_min, x_max)
             cy = random.randint(y_min, y_max)
         return cx, cy
 
     def _crop_and_transform(self, cx, cy, input_img, target_arr, randomize=False):
+        target_tile = None
+        transform_target = False if target_arr is None else True
         if randomize:
             angle = 0.
             hflip = random.random() < 0.5
@@ -315,30 +370,36 @@ class SealionDataset(data.Dataset):
 
         crop_w, crop_h = get_crop_size(self.tile_size[0], self.tile_size[1], angle, scale)
         input_tile = crop_around(input_img, cx, cy, crop_w, crop_h)
-        target_tile = crop_around(target_arr, cx, cy, crop_w, crop_h)
+        if transform_target:
+            target_tile = crop_around(target_arr, cx, cy, crop_w, crop_h)
 
-        Mtrans = np.identity(3)
-        Mtrans[0, 2] = (self.tile_size[0] - crop_w) // 2
-        Mtrans[1, 2] = (self.tile_size[1] - crop_h) // 2
-        if hflip:
-            Mtrans[0, 0] *= -1
-            Mtrans[0, 2] = self.tile_size[0] - Mtrans[0, 2]
-        if vflip:
-            Mtrans[1, 1] *= -1
-            Mtrans[1, 2] = self.tile_size[1] - Mtrans[1, 2]
+        # Perform tile geometry transforms if needed
+        if angle or scale != 1. or hflip or vflip:
+            Mtrans = np.identity(3)
+            Mtrans[0, 2] = (self.tile_size[0] - crop_w) // 2
+            Mtrans[1, 2] = (self.tile_size[1] - crop_h) // 2
+            if hflip:
+                Mtrans[0, 0] *= -1
+                Mtrans[0, 2] = self.tile_size[0] - Mtrans[0, 2]
+            if vflip:
+                Mtrans[1, 1] *= -1
+                Mtrans[1, 2] = self.tile_size[1] - Mtrans[1, 2]
 
-        if angle or scale != 1.:
-            Mrot = cv2.getRotationMatrix2D((crop_w//2, crop_h//2), angle, scale)
-            Mfinal = np.dot(Mtrans, np.vstack([Mrot, [0, 0, 1]]))
-        else:
-            Mfinal = Mtrans
+            if angle or scale != 1.:
+                Mrot = cv2.getRotationMatrix2D((crop_w//2, crop_h//2), angle, scale)
+                Mfinal = np.dot(Mtrans, np.vstack([Mrot, [0, 0, 1]]))
+            else:
+                Mfinal = Mtrans
 
-        #print(input_tile.shape)
+            #print(input_tile.shape)
+            input_tile = cv2.warpAffine(input_tile, Mfinal[:2, :], tuple(self.tile_size))
+            if transform_target:
+                tt64 = target_tile.astype(np.float64)
+                tt64 = cv2.warpAffine(tt64, Mfinal[:2, :], tuple(self.tile_size))
+                if scale != 1.:
+                    tt64 /= scale**2
+                target_tile = tt64.astype(np.float32)
 
-        input_tile = cv2.warpAffine(input_tile, Mfinal[:2, :], tuple(self.tile_size))
-        target_tile = cv2.warpAffine(target_tile, Mfinal[:2, :], tuple(self.tile_size))
-
-        #print(np.ceil(np.sum(target_tile)/25))
         return input_tile, target_tile
 
     def __getitem__(self, index):
@@ -349,25 +410,58 @@ class SealionDataset(data.Dataset):
             tile = 0  #FIXME sort this out
 
         input_id = self.input_index[index % len(self)]
-        #print(input_id, index, tile)
         input_img = self._load_input(input_id)
-        target_arr = self._load_target(input_id)
-        h, w = input_img.shape[:2]
-        attempts = 32
-        for i in range(attempts):
-            tw, th = self.tile_size
-            cx, cy = self._random_tile_center(input_id, (tw, th, w - tw, h - th))
-            input_tile, target_tile = self._crop_and_transform(cx, cy, input_img, target_arr, randomize=True)
-            # check centre of chosen tile for valid pixels
-            if np.any(crop_around(input_tile, tw//2, th//2, tw//4, th//4)):
-                break
+        #print(input_id, index, tile)
 
-        input_tile_tensor = self.transform(input_tile)
+        if self.train:
+            target_arr = self._load_target(input_id)
+            h, w = input_img.shape[:2]
+            attempts = 32
+            for i in range(attempts):
+                tw, th = self.tile_size
+                cx, cy = self._random_tile_center(input_id, w, h)
+                input_tile, target_tile = self._crop_and_transform(cx, cy, input_img, target_arr, randomize=True)
+                # check centre of chosen tile for valid pixels
+                if np.any(crop_around(input_tile, tw//2, th//2, tw//4, th//4)):
+                    break
+            input_tile_tensor = self.transform(input_tile)
+            target_tile_tensor = to_tensor(target_tile)
+        else:
+            target_arr = None
+            if self.has_targets:
+                target_arr = self._load_target(input_id)
+            cx, cy = self._indexed_tile_center(input_id, index)
+            input_tile, target_tile = self._crop_and_transform(cx, cy, input_img, target_arr, randomize=False)
+            input_tile_tensor = self.transform(input_tile)
+            if target_tile is None:
+                target_tile_tensor = torch.from_numpy(np.array([input_id, tile]))
+            else:
+                target_tile_tensor = to_tensor(target_tile)
+            #print(input_tile_tensor.size(), target_tile_tensor)
 
         #cv2.imwrite('test-scaled-input-%d.png' % index, input_tile)
         #cv2.imwrite('test-scaled-target-%d.png' % index, 4096*target_tile[:, :, :3])
 
-        return input_tile_tensor, to_tensor(target_tile)
+        return input_tile_tensor, target_tile_tensor
 
     def __len__(self):
         return len(self.input_index)
+
+
+def combine_patches(output_img, patches, patch_size, stride, agg_fn=np.mean):
+    oh, ow = output_img.shape[:2]
+    pw, ph = patch_size
+    patch_rows = ow // pw + 1
+    for i in range(0, ow):
+        pi_left = ((i - pw) // stride) + 1
+        pi_right = i // stride + 1
+        for j in range(0, oh):
+            pj_top = ((j - ph) // stride) + 1
+            pj_bottom = j // stride + 1
+            agg = []
+            for pi in range(pi_left, pi_right):
+                for pj in range(pj_top, pj_bottom):
+                    px = i - pi * stride
+                    py = j - pj * stride
+                    agg.append(patches[pi + pj * patch_rows][px, py])
+            output_img[i, j] = agg_fn(agg)

models/train.py
@@ -5,12 +5,14 @@ import shutil
 from dataset import SealionDataset, RandomTileSampler
 from model_cnet import ModelCnet
 from model_countception import ModelCountception
+from utils import AverageMeter
 
 import torch
 import torch.autograd as autograd
 import torch.utils.data as data
 import torch.optim as optim
 import torch.nn.functional as F
+import torchvision.utils
 
 parser = argparse.ArgumentParser(description='PyTorch Sealion count training')
 parser.add_argument('data', metavar='DIR',
@@ -35,31 +37,18 @@ parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA training')
 parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
+parser.add_argument('--save-batches', action='store_true', default=False,
+                    help='save images of batch inputs and targets every log interval for debugging/verification')
 
-
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
 
 
 def main():
     args = parser.parse_args()
 
-    train_input_root = os.path.join(args.data, 'Train-processed/inputs')
-    train_target_root = os.path.join(args.data, 'Train-processed/targets')
+    processed_base = 'Train-processed-2'
+    train_input_root = os.path.join(args.data, processed_base, 'inputs')
+    train_target_root = os.path.join(args.data,  processed_base, 'targets')
+    train_process_file = os.path.join(args.data, processed_base, 'process.csv')
     train_counts_file = os.path.join(args.data, 'Train/train.csv')
     train_coords_file = os.path.join(args.data, 'Train/correct_coords.csv')
 
@@ -71,6 +60,8 @@ def main():
         train_target_root,
         train_counts_file,
         train_coords_file,
+        train_process_file,
+        train=True,
         tile_size=tile_size)
     sampler = RandomTileSampler(dataset, oversample=256, repeat=16)
     loader = data.DataLoader(
@@ -98,7 +89,7 @@ def main():
 
     for epoch in range(1, num_epochs + 1):
         adjust_learning_rate(optimizer, epoch, initial_lr=args.lr, decay_epochs=3)
-        train_epoch(epoch, model, loader, optimizer, loss_fn, no_cuda=args.no_cuda)
+        train_epoch(epoch, model, loader, optimizer, loss_fn, args)
         save_checkpoint({
             'epoch': epoch + 1,
             'arch': 'c-net',
@@ -109,7 +100,7 @@ def main():
             filename='checkpoint-%d.pth.tar' % epoch)
 
 
-def train_epoch(epoch, model, loader, optimizer, loss_fn, log_interval=10, no_cuda=False):
+def train_epoch(epoch, model, loader, optimizer, loss_fn, args):
     batch_time_m = AverageMeter()
     data_time_m = AverageMeter()
     losses_m = AverageMeter()
@@ -119,7 +110,7 @@ def train_epoch(epoch, model, loader, optimizer, loss_fn, log_interval=10, no_cu
     end = time.time()
     for batch_idx, (input, target) in enumerate(loader):
         data_time_m.update(time.time() - end)
-        if no_cuda:
+        if args.no_cuda:
             input_var, target_var = autograd.Variable(input), autograd.Variable(target)
         else:
             input_var, target_var = autograd.Variable(input.cuda()), autograd.Variable(target.cuda())
@@ -133,7 +124,7 @@ def train_epoch(epoch, model, loader, optimizer, loss_fn, log_interval=10, no_cu
         optimizer.step()
 
         batch_time_m.update(time.time() - end)
-        if batch_idx % log_interval == 0:
+        if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]  '
                   'Loss: {loss.val:.6f} ({loss.avg:.4f})  '
                   'Time: {batch_time.val:.3f}s, {rate:.3f}/s  '
@@ -147,6 +138,10 @@ def train_epoch(epoch, model, loader, optimizer, loss_fn, log_interval=10, no_cu
                 rate=input_var.size(0) / batch_time_m.val,
                 rate_avg=input_var.size(0) / batch_time_m.avg,
                 data_time=data_time_m))
+
+            if args.save_batches:
+                torchvision.utils.save_image(input, 'input-batch-%d.jpg' % batch_idx, normalize=True)
+                torchvision.utils.save_image(torch.sum(target, dim=1), 'target-batch-%d.jpg' % batch_idx, normalize=True)
         end = time.time()
 
 

scripts/process_train.py
@@ -11,9 +11,8 @@ from scipy.ndimage import gaussian_filter
 
 
 COLS = ['filename', 'width', 'height', 'buffer_width', 'buffer_height', 'x_offset', 'y_offset']
-
-categories = ["adult_males",  "subadult_males", "adult_females", "juveniles",  "pups"]
-category_map = {"adult_males": 0, "subadult_males": 1, "adult_females": 2, "juveniles": 3, "pups": 4}
+CATEGORIES = ["adult_males", "subadult_males", "adult_females", "juveniles", "pups"]
+CATEGORY_MAP = {"adult_males": 0, "subadult_males": 1, "adult_females": 2, "juveniles": 3, "pups": 4}
 
 
 def get_outdir(parent_dir, child_dir=''):
@@ -34,17 +33,17 @@ def find_images(folder, types=('.jpg', '.jpeg')):
 
 
 class Process(object):
-    def __init__(self, root_path, counts_filename, coords_filename):
+    def __init__(self, root_path, counts_filename, coords_filename, dest_name='Train-processed'):
 
         self.input_path = os.path.join(root_path, 'Train')
         self.dotted_path = os.path.join(root_path, 'TrainDotted')
-        self.output_path_inputs = get_outdir(os.path.join(root_path, 'Train-processed', 'inputs'))
-        self.output_path_targets = get_outdir(os.path.join(root_path, 'Train-processed', 'targets'))
+        self.output_path_inputs = get_outdir(os.path.join(root_path, dest_name, 'inputs'))
+        self.output_path_targets = get_outdir(os.path.join(root_path, dest_name, 'targets'))
         self.counts_df = pd.read_csv(os.path.join(root_path, counts_filename), index_col=0)
         self.coords_df = pd.read_csv(os.path.join(root_path, coords_filename), index_col=False)
         self.coords_df.x_coord = self.coords_df.x_coord.astype('int')
         self.coords_df.y_coord = self.coords_df.y_coord.astype('int')
-        self.coords_df.category = self.coords_df.category.replace(category_map)
+        self.coords_df.category = self.coords_df.category.replace(CATEGORY_MAP)
         self.coords_by_file = self.coords_df.groupby('filename')
         self.bsize = 256
         self.reflect = False
@@ -116,37 +115,25 @@ class Process(object):
 
         img = cv2.copyMakeBorder(img, y_offset, y_diff-y_offset, x_offset, x_diff-x_offset, border, value)
         cv2.imwrite(os.path.join(self.output_path_inputs, frel), img)
-        cv2.imwrite(os.path.join(self.output_path_inputs, basename + '.png'), img)
+        #cv2.imwrite(os.path.join(self.output_path_inputs, basename + '.png'), img)
 
         print(self.counts_df.ix[fid])
         yxc = self.coords_by_file.get_group(frel).as_matrix(columns=['y_coord', 'x_coord', 'category'])
 
-        for cat_name in categories:
-            cat_idx = category_map[cat_name]
+        targets = []
+        write_scaled_pngs = False
+        for cat_idx, cat_name in enumerate(CATEGORIES):
             yx = yxc[yxc[:, 2] == cat_idx][:, :2]
             yx += [y_offset, x_offset]
 
             gimg = np.zeros([hb, wb])
             for y, x in yx:
-                gimg[y, x] += 32.
+                gimg[y, x] += 1024.
 
             # OpenCV gaussian blur
             target_img = cv2.GaussianBlur(gimg, (19, 19), 3, borderType=cv2.BORDER_REFLECT_101)
             target_img = cv2.bitwise_and(target_img, target_img, mask=mask)
-
-            test_sum = np.sum(target_img) / 32
             print("Min/max: ", np.min(target_img), np.max(target_img))
-
-            UINT16_MAX = np.iinfo(np.uint16).max
-            target_img = target_img * UINT16_MAX
-            target_img = target_img.astype('uint16')
-
-            target_path = os.path.join(self.output_path_targets, basename + '-target-%d.png' % cat_idx)
-            cv2.imwrite(target_path, target_img)
-
-            # read back and verify
-            target_img_float = cv2.imread(target_path, -1) / UINT16_MAX
-            target_img_float_sum = np.sum(target_img_float) / 32
 
             # Scipy, scipy.ndimage.filters.gaussian_filter
             #blah2 = gaussian_filter(gimg, 3)
@@ -156,8 +143,26 @@ class Process(object):
             #blah2 = blah2 * 255
             #cv2.imwrite(os.path.join(self.output_path, basename + '-gn.jpg'), blah2)
 
-            print('Counts for class %d:' % cat_idx, test_sum, target_img_float_sum)
-            assert(np.isclose(test_sum, target_img_float_sum, rtol=.001, atol=.001))
+            targets.append(target_img.astype(np.float32))
+            test_sum = np.sum(target_img) / 1024
+            if write_scaled_pngs:
+                INT_SCALE = np.iinfo(np.uint16).max / 32
+                target_img_uint16 = target_img * INT_SCALE
+                target_img_uint16 = target_img_uint16.astype('uint16')
+
+                target_path = os.path.join(self.output_path_targets, basename + '-target-%d.png' % cat_idx)
+                cv2.imwrite(target_path, target_img_uint16)
+
+                # read back and verify
+                target_img_float = cv2.imread(target_path, -1) / INT_SCALE
+                target_img_float_sum = np.sum(target_img_float) / 1024
+
+                print('Counts for class %d:' % cat_idx, test_sum, target_img_float_sum, len(yx))
+                assert(np.isclose(test_sum, target_img_float_sum, rtol=.001, atol=.001))
+
+        target_stacked = np.dstack(targets)
+        target_path = os.path.join(self.output_path_targets, basename + '-target.npz')
+        np.savez_compressed(target_path, target_stacked)
 
         results.append(result)
 
@@ -183,14 +188,15 @@ def main():
     args = parser.parse_args()
 
     root_path = '/data/x/sealion/'
+    dest_name = 'Train_processed'
     counts_filename = 'Train/train.csv'
     coords_filename = 'Train/correct_coords.csv'
 
-    process = Process(root_path, counts_filename, coords_filename)
+    process = Process(root_path, counts_filename, coords_filename, dest_name=dest_name)
     results = process()
 
     df = pd.DataFrame.from_records(results, columns=COLS)
-    df.to_csv(os.path.join(root_path, 'Train-processed', 'info.csv'), index=False)
+    df.to_csv(os.path.join(root_path, dest_name, 'processed.csv'), index=False)
 
 if __name__ == '__main__':
     main()