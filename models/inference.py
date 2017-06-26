bd48e3d698740ef3dd3f4682f12fae7b985e6ec7
Ross Wightman <rwightman@gmail.com>
2017-06-26 16:04:27 -0700

    Add faster/less problematic non-overlapped inference mode + inference stats

--- models/inference.py
+++ models/inference.py
@@ -23,7 +23,7 @@ parser.add_argument('--batch-size', type=int, default=16, metavar='N',
                     help='input batch size for training (default: 16)')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                     help='how many batches to wait before logging training status')
 parser.add_argument('--num-processes', type=int, default=2, metavar='N',
                     help='how many training processes to use (default: 2)')
@@ -44,11 +44,13 @@ def main():
     batch_size = args.batch_size
     patch_size = [384] * 2
     num_outputs = 5
+    overlapped_patches = False
     dataset = SealionDataset(
         test_input_root,
         processing_file=processed_file,
         train=False,
         patch_size=patch_size,
+        patch_stride=patch_size[0] // 2 if overlapped_patches else patch_size[0],
         per_image_norm=True)
     sampler = IndexedPatchSampler(dataset)
     loader = data.DataLoader(
@@ -71,11 +73,13 @@ def main():
 
     batch_time_m = AverageMeter()
     data_time_m = AverageMeter()
-    end = time.time()
+    model_time_m = AverageMeter()
+    post_time_m = AverageMeter()
     current_id = -1
     patches = []
     results = []
     try:
+        end = time.time()
         for batch_idx, (input, target, index) in enumerate(loader):
             data_time_m.update(time.time() - end)
             if not args.no_cuda:
@@ -83,32 +87,58 @@ def main():
             else:
                 input_var, target_var = autograd.Variable(input), autograd.Variable(target)
             output = model(input_var)
-            output = output.permute(0, 2, 3, 1)
+            output = output.permute(0, 2, 3, 1) / 1024.
+            if not overlapped_patches:
+                output = torch.squeeze(output.sum(dim=1))
+                output = torch.squeeze(output.sum(dim=1))
             output = output.cpu().data.numpy()
 
             for result_index, o in zip(index, output):
                 input_id, index, patch_index = result_index
                 #print('input_id, index, patch_index: ', input_id, index, patch_index)
+
                 if current_id == -1:
                     current_id = input_id
                 elif current_id != input_id:
-                    # reconstruct output image from patches
-                    w, h = dataset.get_input_size(current_id)
-                    cols = dataset.get_patch_cols(current_id)
-                    output_arr = np.zeros((h, w, num_outputs), dtype=np.float32)
-                    patches_arr = np.stack(patches)
-                    merge_patches_float32(output_arr, patches_arr, cols, dataset.patch_size, dataset.patch_stride)
-                    counts = list(np.sum(output_arr, axis=(0, 1)) / 1024.)
+                    if overlapped_patches:
+                        # reconstruct output image from overlapping patches
+                        w, h = dataset.get_input_size(current_id)
+                        cols = dataset.get_patch_cols(current_id)
+                        output_arr = np.zeros((h, w, num_outputs), dtype=np.float32)
+                        patches_arr = np.stack(patches)
+                        merge_patches_float32(output_arr, patches_arr, cols, dataset.patch_size, dataset.patch_stride)
+                        counts = list(np.sum(output_arr, axis=(0, 1)))
+                        debug_image = False
+                        if debug_image:
+                            debug_img = cv2.normalize(
+                                output_arr[:, :, :3], None, 0, 255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC3)
+                            cv2.imwrite('output-%d.png' % current_id, debug_img)
+                    else:
+                        #print(len(patches))
+                        counts = list(np.sum(patches, axis=0))
                     print(counts)
-                    debug_image = True
-                    if debug_image:
-                        debug_img = cv2.normalize(
-                            output_arr[:, :, :3], None, 0, 255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC3)
-                        cv2.imwrite('output-%d.png' % current_id, debug_img)
                     results.append([current_id] + counts)
                     patches = []
                     current_id = input_id
+
                 patches.append(o)
+                # end iterating through batch
+
+            batch_time_m.update(time.time() - end)
+            if batch_idx % args.log_interval == 0:
+                print('Inference: [{}/{} ({:.0f}%)]  '
+                      'Time: {batch_time.val:.3f}s, {rate:.3f}/s  '
+                      '({batch_time.avg:.3f}s, {rate_avg:.3f}/s)  '
+                      'Data: {data_time.val:.3f} ({data_time.avg:.3f})'.format(
+                    batch_idx * len(input), len(loader.sampler),
+                    100. * batch_idx / len(loader),
+                    batch_time=batch_time_m,
+                    rate=input_var.size(0) / batch_time_m.val,
+                    rate_avg=input_var.size(0) / batch_time_m.avg,
+                    data_time=data_time_m))
+
+            end = time.time()
+            #end iterating through dataset
     except KeyboardInterrupt:
         pass
     results_df = pd.DataFrame(results, columns=COLS)
